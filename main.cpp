3e44404fd2fd9ff95dbcb9b57d7f1e5bfcbf5d66
ppdx999 <ppdx999@gmail.com>
2025-03-02 17:35:50 +0900

    update template

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <bits/stdc++.h>
+#include <vector>
 using namespace std;
+
+#define rep1(i, n) for(int i = 0; i < (n); ++i)
+#define rep2(i, a, b) for(int i = (a); i < (b); ++i)
+#define rep3(i, a, b, s) for(int i = (a); i < (b); i += (s))
+
+#define drep1(i, n) for(int i = (n) - 1; i >= 0; --i)
+#define drep2(i, a, b) for(int i = (b) - 1; i >= (a); --i)
+#define drep3(i, a, b, s) for(int i = (b) - 1; i >= (a); i -= (s))
+
+#define GET_MACRO(_1, _2, _3, _4, NAME, ...) NAME
+#define rep(...) GET_MACRO(__VA_ARGS__, rep3, rep2, rep1)(__VA_ARGS__)
+#define drep(...) GET_MACRO(__VA_ARGS__, drep3, drep2, drep1)(__VA_ARGS__)
+
+#define pb push_back
+#define mp make_pair
+#define fst first
+#define snd second
+
+typedef long long ll;
 
 int main() {
 }
