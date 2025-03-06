a22f2aef784505a9302f705cd0e557896897e3a5
ppdx999 <ppdx999@gmail.com>
2025-03-06 14:12:34 +0900

    solve tessoku/a65

---
+++ tessoku/a65/main.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+#define rep1(i, n) for(int i = 0; i < (n); ++i)
+#define rep2(i, a, b) for(int i = (a); i < (b); ++i)
+#define rep3(i, a, b, s) for(int i = (a); i < (b); i += (s))
+
+#define REP1(i, n) for(int i = 0; i <= (n); ++i)
+#define REP2(i, a, b) for(int i = (a); i <= (b); ++i)
+#define REP3(i, a, b, s) for(int i = (a); i <= (b); i += (s))
+
+#define drep1(i, n) for(int i = (n) - 1; i >= 0; --i)
+#define drep2(i, a, b) for(int i = (b) - 1; i >= (a); --i)
+#define drep3(i, a, b, s) for(int i = (b) - 1; i >= (a); i -= (s))
+
+#define DREP1(i, n) for(int i = (n); i >= 0; --i)
+#define DREP2(i, a, b) for(int i = (b); i >= (a); --i)
+#define DREP3(i, a, b, s) for(int i = (b); i >= (a); i -= (s))
+
+#define GET_MACRO(_1, _2, _3, _4, NAME, ...) NAME
+#define rep(...) GET_MACRO(__VA_ARGS__, rep3, rep2, rep1)(__VA_ARGS__)
+#define REP(...) GET_MACRO(__VA_ARGS__, REP3, REP2, REP1)(__VA_ARGS__)
+#define drep(...) GET_MACRO(__VA_ARGS__, drep3, drep2, drep1)(__VA_ARGS__)
+#define DREP(...) GET_MACRO(__VA_ARGS__, DREP3, DREP2, DREP1)(__VA_ARGS__)
+
+constexpr int INF = std::numeric_limits<int>::max();
+typedef long long ll;
+
+vector<int> G[100001];
+int dp[100001];
+
+int main() {
+	int n; cin >> n;
+	REP(i, 2, n) {
+		int a; cin >> a;
+		G[a].push_back(i);
+	}
+	DREP(i, 1, n){
+		dp[i] = 0;
+		rep(j, G[i].size()) {
+			dp[i] += (dp[G[i][j]] + 1);
+		}
+	}
+
+	REP(i, 1, n) {
+		if (i > 1) cout << " ";
+		cout << dp[i];
+	}
+	cout << endl;
+	return 0;
+}
