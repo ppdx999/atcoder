5e62fd0d41bfb2d1d01c288e239fefc0fc6f558d
fujis <ppdx999@gmail.com>
2025-04-07 19:45:15 +0900

    add golang template

---
+++ a.out.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var r = bufio.NewReader(os.Stdin)
+var w = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer w.Flush()
+
+	var n int
+	fmt.Fscan(r, &n)
+
+	fmt.Fprintln(w, n)
+}
