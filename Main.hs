6ba0c0900fae715e5d3a381cb383948643192652
fujis <ppdx999@gmail.com>
2025-05-13 19:04:14 +0900

    update template

--- Main.hs
+++ Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Main (main) where
 
 import Data.ByteString.Char8 qualified as BS
@@ -7,14 +9,15 @@ import GHC.Unicode (isSpace)
 ints :: IO [Int]
 ints = L.unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
 
+ints2 :: IO (Int, Int)
+ints2 =
+  ints >>= \case
+    [x1, x2] -> return (x1, x2)
+    _ -> error "ints2: wrong number of integers"
+
 yn :: Bool -> String
 yn True = "Yes"
 yn False = "No"
 
 printYn :: Bool -> IO ()
 printYn = putStrLn . yn
-
-main :: IO ()
-main = do
-  (n : _) <- ints
-  print (n * n)
