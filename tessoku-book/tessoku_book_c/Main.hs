647c168380453fcd7206e61b4ed1a62542da5b1a
fujis <ppdx999@gmail.com>
2025-05-13 15:33:26 +0900

    solve

---
+++ tessoku-book/tessoku_book_c/Main.hs
@@ -0,0 +1,22 @@
+module Main (main) where
+
+import Data.ByteString.Char8 qualified as BS
+import Data.List (find)
+import Data.List qualified as L
+import GHC.Unicode (isSpace)
+
+ints :: IO [Int]
+ints = L.unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
+
+yn True = "Yes"
+yn False = "No"
+
+printYn = putStrLn . yn
+
+main :: IO ()
+main = do
+  (n : k : _) <- ints
+  ps <- ints
+  ks <- ints
+
+  printYn $ elem k [x + y | x <- ps, y <- ks]
