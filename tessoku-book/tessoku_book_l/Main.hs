992972fba91ede73f48a5072b38b0a816c2f7cde
fujis <ppdx999@gmail.com>
2025-05-16 22:34:09 +0900

    solve tessoku-book/tessoku_book_l

---
+++ tessoku-book/tessoku_book_l/Main.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE LambdaCase #-}
+
+module Main (main) where
+
+import Data.ByteString.Char8 qualified as BS
+import Data.List qualified as L
+import GHC.Unicode (isSpace)
+
+-- ///////////////////////////////////////////////////////////////////////
+-- TEMPLATE
+-- ///////////////////////////////////////////////////////////////////////
+
+ints :: IO [Int]
+ints = L.unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
+
+ints1 :: IO Int
+ints1 =
+  ints >>= \case
+    [x1] -> return x1
+    _ -> error "ints1: wrong number of integers"
+
+ints2 :: IO (Int, Int)
+ints2 =
+  ints >>= \case
+    [x1, x2] -> return (x1, x2)
+    _ -> error "ints2: wrong number of integers"
+
+ints3 :: IO (Int, Int, Int)
+ints3 =
+  ints >>= \case
+    [x1, x2, x3] -> return (x1, x2, x3)
+    _ -> error "ints3: wrong number of integers"
+
+ints4 :: IO (Int, Int, Int, Int)
+ints4 =
+  ints >>= \case
+    [x1, x2, x3, x4] -> return (x1, x2, x3, x4)
+    _ -> error "ints4: wrong number of integers"
+
+yn :: Bool -> String
+yn True = "Yes"
+yn False = "No"
+
+printYn :: Bool -> IO ()
+printYn = putStrLn . yn
+
+-- ///////////////////////////////////////////////////////////////////////
+-- Main
+-- ///////////////////////////////////////////////////////////////////////
+
+check :: [Int] -> Int -> Int -> Bool
+check as k n = k <= sum (map (n `div`) as)
+
+binSearch :: [Int] -> Int -> Int -> Int -> Int
+binSearch as ng ok k
+  | ok - ng > 1 =
+      let mid = (ok + ng) `div` 2
+          (ng', ok') =
+            if check as k mid
+              then (ng, mid)
+              else (mid, ok)
+       in binSearch as ng' ok' k
+  | otherwise = ok
+
+main :: IO ()
+main = do
+  (n, k) <- ints2
+  as <- ints
+  print $ binSearch as 0 1000000001 k
