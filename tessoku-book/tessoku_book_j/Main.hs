68626bf9341ead45d26112326bec43b51dfb6ac8
fujis <ppdx999@gmail.com>
2025-05-17 01:31:54 +0900

    solve tessoku-book/tessoku_book_j

--- tessoku-book/tessoku_book_j/Main.hs
+++ tessoku-book/tessoku_book_j/Main.hs
@@ -28,25 +28,6 @@ ints2 =
     [x1, x2] -> return (x1, x2)
     _ -> error "ints2: wrong number of integers"
 
-ints3 :: IO (Int, Int, Int)
-ints3 =
-  ints >>= \case
-    [x1, x2, x3] -> return (x1, x2, x3)
-    _ -> error "ints3: wrong number of integers"
-
-ints4 :: IO (Int, Int, Int, Int)
-ints4 =
-  ints >>= \case
-    [x1, x2, x3, x4] -> return (x1, x2, x3, x4)
-    _ -> error "ints4: wrong number of integers"
-
-yn :: Bool -> String
-yn True = "Yes"
-yn False = "No"
-
-printYn :: Bool -> IO ()
-printYn = putStrLn . yn
-
 -- ///////////////////////////////////////////////////////////////////////
 -- Main
 -- ///////////////////////////////////////////////////////////////////////
