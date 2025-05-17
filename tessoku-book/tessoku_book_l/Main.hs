4ddd2fa3a57511ec0ca94229cc7b07cccb1737fd
fujis <ppdx999@gmail.com>
2025-05-17 11:31:57 +0900

    fixup

--- tessoku-book/tessoku_book_l/Main.hs
+++ tessoku-book/tessoku_book_l/Main.hs
@@ -13,36 +13,11 @@ import GHC.Unicode (isSpace)
 ints :: IO [Int]
 ints = L.unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
 
-ints1 :: IO Int
-ints1 =
-  ints >>= \case
-    [x1] -> return x1
-    _ -> error "ints1: wrong number of integers"
-
 ints2 :: IO (Int, Int)
 ints2 =
   ints >>= \case
     [x1, x2] -> return (x1, x2)
     _ -> error "ints2: wrong number of integers"
-
-ints3 :: IO (Int, Int, Int)
-ints3 =
-  ints >>= \case
-    [x1, x2, x3] -> return (x1, x2, x3)
-    _ -> error "ints3: wrong number of integers"
-
-ints4 :: IO (Int, Int, Int, Int)
-ints4 =
-  ints >>= \case
-    [x1, x2, x3, x4] -> return (x1, x2, x3, x4)
-    _ -> error "ints4: wrong number of integers"
-
-yn :: Bool -> String
-yn True = "Yes"
-yn False = "No"
-
-printYn :: Bool -> IO ()
-printYn = putStrLn . yn
 
 -- ///////////////////////////////////////////////////////////////////////
 -- Main
