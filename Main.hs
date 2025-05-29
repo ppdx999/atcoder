c3220512608002064d12f07c40dbb97e8830b649
fujis <ppdx999@gmail.com>
2025-05-29 16:43:27 +0900

    テンプレート更新

--- Main.hs
+++ Main.hs
@@ -2,14 +2,13 @@
 
 module Main (main) where
 
-import Data.ByteString.Char8 (dropWhile, getLine, readInt)
+import Data.ByteString.Char8 qualified as BS
 import Data.List (unfoldr)
 import GHC.Unicode (isSpace)
-import Prelude hiding (dropWhile, getLine)
 
 -- | 標準入力の1行をスペース区切りで文字列のリストに分割する
 ints :: IO [Int]
-ints = unfoldr (readInt . dropWhile isSpace) <$> getLine
+ints = unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
 
 ints1 :: IO Int
 ints1 =
