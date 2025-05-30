73dc35dbb5d2d9146aebb9730c1a9ac834c44f9c
fujis <ppdx999@gmail.com>
2025-05-30 16:43:21 +0900

    solve tessoku-book/tessoku_book_d

--- tessoku-book/tessoku_book_d/Main.hs
+++ tessoku-book/tessoku_book_d/Main.hs
@@ -1,25 +1,15 @@
-module Main (main) where
-
-import Data.ByteString.Char8 qualified as BS
-import Data.List qualified as L
-import GHC.Unicode (isSpace)
-
 ints :: IO [Int]
-ints = L.unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
+ints = map read . words <$> getLine
 
-yn :: Bool -> String
-yn True = "Yes"
-yn False = "No"
-
-printYn :: Bool -> IO ()
-printYn = putStrLn . yn
-
-toBinary :: Int -> String
 toBinary 0 = ""
 toBinary n = toBinary (n `div` 2) ++ show (n `mod` 2)
 
-main :: IO ()
+padding10 str = replicate (10 - length str) '0' ++ str
+
+solve = padding10 . toBinary
+
 main = do
+  -- 入力
   (n : _) <- ints
-  let bin = toBinary n
-  putStrLn (replicate (10 - length bin) '0' ++ bin)
+  -- 出力
+  putStrLn $ solve n
