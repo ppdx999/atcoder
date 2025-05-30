1117b7b8855b573c55c622a6259b4fcbc230c716
fujis <ppdx999@gmail.com>
2025-05-30 16:50:58 +0900

    solve tessoku-book/tessoku_book_e

--- tessoku-book/tessoku_book_e/Main.hs
+++ tessoku-book/tessoku_book_e/Main.hs
@@ -1,20 +1,7 @@
-module Main (main) where
-
-import Data.ByteString.Char8 qualified as BS
-import Data.List qualified as L
-import GHC.Unicode (isSpace)
-
-ints :: IO [Int]
-ints = L.unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
-
-within :: Int -> Int -> [Int] -> [Int]
-within l r = filter (\x -> l <= x && x <= r)
-
-main :: IO ()
 main = do
-  (n : k : _) <- ints
+  [n, k] <- map read . words <$> getLine
   let red = [1 .. n]
       blue = [1 .. n]
       possibleAnswers = [k - x - y | x <- red, y <- blue]
-      countUpAnswer = length . within 1 n
+      countUpAnswer = length . filter (\x -> 1 <= x && x <= n)
   print $ countUpAnswer possibleAnswers
