054704044f28135534103858066063708e777318
fujis <ppdx999@gmail.com>
2025-05-30 17:29:37 +0900

    solve tessoku-book/math_and_algorithm_ai

--- tessoku-book/math_and_algorithm_ai/Main.hs
+++ tessoku-book/math_and_algorithm_ai/Main.hs
@@ -1,33 +1,35 @@
-{-# LANGUAGE LambdaCase #-}
-
-module Main (main) where
-
 import Control.Monad (replicateM)
 import Data.Array.Unboxed (Array, listArray, (!))
-import Data.ByteString.Char8 qualified as BS
 import Data.Functor ((<&>))
-import Data.List qualified as L
-import GHC.Unicode (isSpace)
 
+-- | 入力から数値のリストを受け取る
 ints :: IO [Int]
-ints = L.unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
+ints = map read . words <$> getLine
 
-ints2 :: IO (Int, Int)
-ints2 =
-  ints >>= \case
-    [x1, x2] -> return (x1, x2)
-    _ -> error "ints2: wrong number of integers"
+ints2 = ints <&> \[x1, x2] -> (x1, x2)
 
-csum :: [Int] -> [Int]
-csum = L.scanl' (+) 0
+-- | 一次元累積和を計算する
+cum1D :: [Int] -> [Int]
+cum1D = scanl1 (+)
 
 toArray :: [Int] -> Array Int Int
-toArray as = listArray (0, length as - 1) as
+toArray as = listArray (0, length as) (0 : as)
 
-main :: IO ()
+solve n q as qs = nGuests
+  where
+    -- ゲストの数 :: [Int]
+    nGuests = map count qs
+
+    -- l~r日の来場者を数える
+    count (l, r) = (arr ! r) - (arr ! pred l)
+
+    -- Ai (1~N)の累積和
+    arr = toArray $ cum1D as
+
 main = do
+  -- 入力
   (n, q) <- ints2
-  as <- ints <&> (toArray . csum)
+  as <- ints
   qs <- replicateM q ints2
-  let countGuests (l, r) = (as ! r) - (as ! pred l)
-   in mapM_ (print . countGuests) qs
+  -- 出力
+  mapM_ print $ solve n q as qs
