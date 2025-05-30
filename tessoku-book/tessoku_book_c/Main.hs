26b892f7daebea14541b97bdbbbf60b5c5ecefa6
fujis <ppdx999@gmail.com>
2025-05-30 16:38:48 +0900

    solve tessoku-book/tessoku_book_c

--- tessoku-book/tessoku_book_c/Main.hs
+++ tessoku-book/tessoku_book_c/Main.hs
@@ -1,22 +1,13 @@
-module Main (main) where
-
-import Data.ByteString.Char8 qualified as BS
-import Data.List (find)
-import Data.List qualified as L
-import GHC.Unicode (isSpace)
-
 ints :: IO [Int]
-ints = L.unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
+ints = map read . words <$> getLine
 
 yn True = "Yes"
 yn False = "No"
 
-printYn = putStrLn . yn
-
 main :: IO ()
 main = do
-  (n : k : _) <- ints
+  [n, k] <- ints
   ps <- ints
   qs <- ints
 
-  printYn $ k `elem` [p + q | p <- ps, q <- qs]
+  putStrLn . yn $ k `elem` [p + q | p <- ps, q <- qs]
