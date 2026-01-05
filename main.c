fc5ee62c4321fb7117fae0bad60ed62b40e2ff3a
ppdx999 <ppdx999@gmail.com>
2026-01-05 21:40:06 +0900

    競プロはCでいく

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#define rep(i, n) for (int i = 0; i < (n); i++)
+static inline  int max(int a, int b) { return a > b ? a : b; }
+static inline  int min(int a, int b) { return a < b ? a : b; }
+
+int main() {
+	return 0;
+}
